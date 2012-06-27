ae69e25b171f3ffcb97c78a7754defff20805244
Christopher Grim <christopher.grim@gmail.com>
2012-06-26 20:34:11 -0400

    Initial Commit

---
+++ set-default-browser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  set-default-browser
+//
+//  Created by Christopher Grim on 6/26/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
