879fd7431fd03b1100a9e3eda5e71cb6afd23a89
Christopher Grim <christopher.grim@gmail.com>
2012-06-27 00:13:18 -0400

    Add a command line tool set default browser for the current user.
    Example usage:
    
      set-default-browser /Applications/Firefox.app
    
    This will set the default handler for the http and https URL schemes
    and the public.html and public.xhtml content types to the specified
    application. AFAICT these are the same changes made by Safari when you
    change the default browser via its preferences.

--- set-default-browser/main.m
+++ set-default-browser/main.m
@@ -3,20 +3,43 @@
 //  set-default-browser
 //
 //  Created by Christopher Grim on 6/26/12.
-//  Copyright (c) 2012 None. All rights reserved.
+//  Copyright (c) 2012 Christopher Grim. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+
+void setDefaultBrowser(NSBundle *bundle)
+{
+    // AFAICT when you change the default browser via Safari's
+    // preferences it changes the default handler for the following
+    // URL schemes:
+    NSArray *schemes = [NSArray arrayWithObjects:@"http", @"https", nil];
+    // and the following content types:
+    NSArray *contentTypes = [NSArray arrayWithObjects:@"public.html", @"public.xhtml", nil];
+
+    NSString *bundleId = [bundle bundleIdentifier];
+    OSStatus result;
+
+    for (NSString *scheme in schemes) {
+        result = LSSetDefaultHandlerForURLScheme((__bridge CFStringRef)scheme,
+                                                 (__bridge CFStringRef)bundleId);
+    }
+
+    for (NSString *contentType in contentTypes) {
+        result = LSSetDefaultRoleHandlerForContentType((__bridge CFStringRef)contentType,
+                                                       kLSRolesAll,
+                                                       (__bridge CFStringRef)bundleId);
+    }
+}
 
 int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSString *path = [[NSString alloc] initWithCString: argv[1]];
+        NSBundle *bundle = [[NSBundle alloc] initWithPath: path];
+        setDefaultBrowser(bundle);
     }
+
     return 0;
 }
-
